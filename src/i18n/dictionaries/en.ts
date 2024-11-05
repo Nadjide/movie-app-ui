85dfe9df3161d569d9192c0a71e897f75b3407ac
Nadjide <nadjide.omar.5@gmail.com>
2024-11-05 10:09:21 +0100

    internatlisation

---
+++ src/i18n/dictionaries/en.ts
@@ -0,0 +1,8 @@
+export const en = {
+    translation: {
+        header: {
+            title: "My movie application",
+            description: "Search and browse among movies of TMDB database",
+        },
+    },
+};
