8efd4c349cc89ac8c67afabb8ffa110b645b201e
Nadjide <nadjide.omar.5@gmail.com>
2024-11-05 10:28:44 +0100

    internallisation fonctionel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
 import { Box, Typography } from "@mui/material";
 import NavBar from "../Navbar/NavBar";
 import classes from "./Header.module.css";
-
+import LanguageSelector from "./LanguageSelector";
+import { useTranslation } from "react-i18next";
+import { i18nMap } from "../../i18n/map";
 export default function Header() {
-  return (
-    <Box className={classes.root}>
-      <Typography component="h1" variant="h5">
-        My movie application
-      </Typography>
-      <Typography variant="body2">
-        Search and browse among movies of TMDB database
-      </Typography>
-      <NavBar />
-    </Box>
-  );
+
+    const { t } = useTranslation();
+    return (
+        <Box className={classes.root}>
+            <Box className={classes.content}>
+                <Typography component="h1" variant="h5">
+                    {t(i18nMap.header.title)}
+                </Typography>
+                <Typography variant="body2">{t(i18nMap.header.description)}</Typography>
+                <NavBar />
+            </Box>
+            <Box>
+                <LanguageSelector />
+            </Box>
+        </Box>
+    );
 }
