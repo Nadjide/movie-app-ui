5fb6a0d24edf2e8077a803374b9ee8607cdf128f
Nadjide <nadjide.omar.5@gmail.com>
2024-11-06 09:16:44 +0100

    Add reflect-metadata and tsyringe for dependency injection; refactor TmdbApiService to use container

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -5,19 +5,23 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import useCurrentLang from "../i18n/hooks/useCurrentLang";
 import { TmdbApiService } from "../services/api/tmdb";
+import { container } from "tsyringe";
 
-const tmdbApiService = new TmdbApiService();
+const tmdbApiService = container.resolve(TmdbApiService);
 
 export default function Movies() {
     const { t } = useTranslation();
     const [searchTerm, setSearchTerm] = useState("");
+
     const currentLang = useCurrentLang((lang: string) =>
         tmdbApiService.search(searchTerm, lang)
     );
+
     const onSearch = (term: string) => {
         setSearchTerm(term);
         tmdbApiService.search(term, currentLang);
     };
+
     return (
         <Box>
             <SearchBar
--- src/services/api/tmdb/index.ts
+++ src/services/api/tmdb/index.ts
@@ -1,11 +1,17 @@
+import { singleton } from "tsyringe";
+
+@singleton()
 export class TmdbApiService {
-    
     private readonly baseUrl = "https://api.themoviedb.org/3";
     private readonly apiKey = "00769e11c3cd17eb5317e3be20ccc9c1";
-    
+
     public search(term: string, lang: string) {
         console.log(lang);
-        fetch(`${this.baseUrl}/search/movie?api_key=${this.apiKey}&language=${lang}&query=${term}&page=1&include_adult=false`)
+
+        fetch(
+            `${this.baseUrl}/search/movie?api_key=${this.apiKey}&language=${lang}
+      &query=${term}&page=1&include_adult=false`
+        )
             .then((r) => r.json())
             .then((d) =>
                 d.results.forEach((m: any) =>
